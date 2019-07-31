f78c2f41638dc53dd91e93b9a1d461e7cd2e7c55
dnl092 <drogalsky14@my.whitworth.edu>
2019-07-30 18:37:30 -0700

    added header

---
+++ src/components/header/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./style.css"
+
+export default function Header() {
+    return (
+        <header>
+            <h1>Memory Game!</h1>
+            <h2>Try to click each image only once</h2>
+        </header>
+    )
+}
