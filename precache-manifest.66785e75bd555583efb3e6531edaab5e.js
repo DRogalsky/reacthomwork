18824af8ccf0bb001007b86cd517ca406bc9212b
dnl092 <drogalsky14@my.whitworth.edu>
2019-08-02 10:04:30 -0700

    Updates

---
+++ precache-manifest.66785e75bd555583efb3e6531edaab5e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0d629e1793d7f4bf76b6064919cdd30",
+    "url": "/reacthomwork/index.html"
+  },
+  {
+    "revision": "aac9a373bd3531d8f244",
+    "url": "/reacthomwork/static/css/main.9dfb84e5.chunk.css"
+  },
+  {
+    "revision": "c65f0670c7d7a243f52e",
+    "url": "/reacthomwork/static/js/2.f75e8bbc.chunk.js"
+  },
+  {
+    "revision": "aac9a373bd3531d8f244",
+    "url": "/reacthomwork/static/js/main.8f1453dc.chunk.js"
+  },
+  {
+    "revision": "99eb4026355bb3cee7c2",
+    "url": "/reacthomwork/static/js/runtime~main.21e1edde.js"
+  },
+  {
+    "revision": "1c0d7348f56920d88cb07c35297edb28",
+    "url": "/reacthomwork/static/media/header-background-training.1c0d7348.png"
+  }
+]);
